7839006217585eec57456539332630a434a490e1
Tiago Oliveira <toliveiracastro93@gmail.com>
2025-08-16 01:04:52 -0300

    chore(backend): add initial backend files

---
+++ houer-challenge-backend/src/main/java/com/example/houer_challenge/HouerChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.houer_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouerChallengeApplication.class, args);
+	}
+
+}
