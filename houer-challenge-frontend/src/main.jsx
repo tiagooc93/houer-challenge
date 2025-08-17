2aa4fad64f0e46bb73fc44f1267d705e52f31b75
Tiago Oliveira <toliveiracastro93@gmail.com>
2025-08-17 04:30:44 -0300

    feat(frontend): protect routes

--- houer-challenge-frontend/src/main.jsx
+++ houer-challenge-frontend/src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Login from "./LoginPage.jsx";
+import Register from "./RegisterPage.jsx";
+import ProtectedRoute from "./ProtectedRoute.jsx";
+import Home from "./Home.jsx";
+import { AuthProvider } from "./AuthContext.jsx";
+import TablePage from "./TablePage.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const token = localStorage.getItem("token");
+const isAuthenticated = token !== null && token !== "";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/Register",
+    element: <Register />,
+  },
+
+  {
+    element: <ProtectedRoute isAuthenticated={isAuthenticated} />, // wraps children
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/table",
+        element: <TablePage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </AuthProvider>
+);
