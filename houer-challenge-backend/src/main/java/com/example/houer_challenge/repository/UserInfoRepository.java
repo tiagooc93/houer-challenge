a1fc9b72521f86e51bea685961fa0f5e532ed8ef
Tiago Oliveira <toliveiracastro93@gmail.com>
2025-08-19 00:18:09 -0300

    feat(backend): add fing by email on user repository

--- houer-challenge-backend/src/main/java/com/example/houer_challenge/repository/UserInfoRepository.java
+++ houer-challenge-backend/src/main/java/com/example/houer_challenge/repository/UserInfoRepository.java
@@ -3,5 +3,10 @@ package com.example.houer_challenge.repository;
 import com.example.houer_challenge.model.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+
+    Optional<UserInfo> findByEmail(String email);
+
 }
