12022abf5e5c9efccd5c212f67b077ac5d457d8e
Tiago Oliveira <toliveiracastro93@gmail.com>
2025-08-19 00:07:49 -0300

    feat(backend): add model for user info

--- houer-challenge-backend/src/main/java/com/example/houer_challenge/model/UserInfo.java
+++ houer-challenge-backend/src/main/java/com/example/houer_challenge/model/UserInfo.java
@@ -1,8 +1,6 @@
 package com.example.houer_challenge.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +14,7 @@ import lombok.Setter;
 public class UserInfo {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
