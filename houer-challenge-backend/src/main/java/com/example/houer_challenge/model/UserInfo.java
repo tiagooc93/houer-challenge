5f3f061f0123bc953acd9cb394e97dffa29f0fa2
Tiago Oliveira <toliveiracastro93@gmail.com>
2025-08-19 03:02:55 -0300

    feat(backend): make user email unique

--- houer-challenge-backend/src/main/java/com/example/houer_challenge/model/UserInfo.java
+++ houer-challenge-backend/src/main/java/com/example/houer_challenge/model/UserInfo.java
@@ -17,7 +17,7 @@ public class UserInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
