3c2314b59113895f3aff31274aa3c69d28dafbd7
Tiago Oliveira <toliveiracastro93@gmail.com>
2025-08-19 00:17:08 -0300

    feat(backend): add new methods to school repository

--- houer-challenge-backend/src/main/java/com/example/houer_challenge/repository/SchoolRepository.java
+++ houer-challenge-backend/src/main/java/com/example/houer_challenge/repository/SchoolRepository.java
@@ -3,5 +3,10 @@ package com.example.houer_challenge.repository;
 import com.example.houer_challenge.model.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SchoolRepository extends JpaRepository<School, Long> {
+    List<School> findAllByOrderByCsvIndexAsc();
+
+    void deleteByIdIn(List<Long> ids);
 }
