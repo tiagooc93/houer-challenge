5621e2f0231974f05cf21a21b6d39e1033cde28a
Tiago Oliveira <toliveiracastro93@gmail.com>
2025-08-19 00:09:55 -0300

    feat(backend): add default user info for tests

--- houer-challenge-backend/src/main/java/com/example/houer_challenge/HouerChallengeApplication.java
+++ houer-challenge-backend/src/main/java/com/example/houer_challenge/HouerChallengeApplication.java
@@ -1,13 +1,26 @@
 package com.example.houer_challenge;
 
+import com.example.houer_challenge.controller.AuthenticationController;
+import com.example.houer_challenge.dto.RegisterDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HouerChallengeApplication {
+public class HouerChallengeApplication  implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HouerChallengeApplication.class, args);
-	}
+    @Autowired
+    AuthenticationController authenticationController;
 
+    public static void main(String[] args) {
+        SpringApplication.run(HouerChallengeApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        authenticationController.register(new RegisterDTO("test@test.com","123"));
+    }
 }
+
